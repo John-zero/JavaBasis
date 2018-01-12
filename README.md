bb66ed8f5419f9ed3120044aa042a7f78fc1987b
John_zero <jzhang@allpaycloud.cn>
2018-01-12 18:31:26 +0800

    结构调整, 增加 System 使用场景分析

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Java 基础迭代加深
 
-com.java.SystemInfo  # System.class, 比如 控制台输出, 系统时间, 主动触发 GC, 数组拷贝, 获取环境变量, 系统属性 ... 等
+com.java.lang._System  # System.class, 比如 控制台输出, 系统时间, 主动触发 GC, 数组内存拷贝, 环境变量, 系统属性 ... 等
---
+++ src/com/java/lang/_System.java
@@ -0,0 +1,56 @@
+package com.java.lang;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Created by John_zero on 2018/1/12.
+ *
+ * 使用场景举例:
+ *  1. GC 回收
+ *      常见: Netty 的 Direct Memory 回收
+ *      主动调用 System.gc(); 触发 FULL GC
+ *      如果JVM 配置参数 -XX:+DisableExplicitGC 会导致 System.gc(); 调用无效
+ *
+ *  2. 对于一些关键配置, 比如 生产环境数据库的账号密码, 生产环境的 Redis 账号密码等 可以使用 环境变量或者系统属性
+ *      常见: Spring (玩的最溜)
+ *          参考: https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#boot-features-external-config
+ *
+ *  3. 数组内存拷贝
+ *      常见: String, ArrayList, Arrays, CopyOnWriteArrayList ...
+ *
+ *  4. ...
+ *      ...
+ *
+ */
+public class _System
+{
+
+    public static void main (String [] args)
+    {
+        // 环境变量
+        Map<String, String> envs = System.getenv();
+
+        for(Map.Entry<String, String> entry : envs.entrySet())
+        {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+        System.out.println("------------------------------------------------------------------------------------------");
+
+        // 系统属性
+        Properties properties = System.getProperties();
+        Set<Object> keySet = properties.keySet();
+        for(Object objectKey : keySet)
+        {
+            Object objectValue = properties.get(objectKey);
+            System.out.println(objectKey + " : " + objectValue);
+        }
+
+        System.out.println("------------------------------------------------------------------------------------------");
+
+
+    }
+
+}
